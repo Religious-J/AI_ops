bffa8a01842bd839eda08a6caa4e15d154f5c2ea
Qianchen Jiang <1569978990@qq.com>
2025-07-18 12:40:32 +0800

    [fix]: fd

--- flash_attention/fd.cpp
+++ flash_attention/fd.cpp
@@ -45,7 +45,7 @@ vector<float> decoder_attention(
     for (size_t i = 0; i < seq_len; ++i) {
         scores[i] = dot_product(q, k_mat[i]) / sqrt(d_k);
     }
-
+ 
     // 2. softmax
     vector<float> attn_weights = softmax(scores);
 
@@ -63,7 +63,7 @@ vector<float> decoder_attention(
 // src: (len_tile,)
 // value: (len_tile, size_per_head)
 // => (size_per_head,)
-std::tuple<vector<float>, vector<float>> onlineSoftmaxWithDot1PASS(
+std::tuple<float, vector<float>> onlineSoftmaxWithDot1PASS(
     std::vector<float> &src, vector<vector<float>> &value) {
 
     size_t len_tile = src.size();
@@ -72,14 +72,18 @@ std::tuple<vector<float>, vector<float>> onlineSoftmaxWithDot1PASS(
     vector<float> lse_vector(size_per_head);
     vector<float> O_local_vector(size_per_head);
 
+    float sum_for_lse = 0.f;
+    for (int i = 0; i < len_tile; i++) {
+        sum_for_lse += std::exp(src[i]);
+    }
+    float lse = logf(sum_for_lse);
+
     for (int loop = 0; loop < size_per_head; loop++) {
         float O_local = 0.f;
         float max_value = -INFINITY;
         float pre_max_value = 0.f;
         float pre_sum = 0.f;
         float sum = 0.f;
-
-        float sum_for_lse = 0.f;
 
         for (int i = 0; i < len_tile; i++) {
           max_value = std::max(max_value, src[i]);
@@ -90,18 +94,11 @@ std::tuple<vector<float>, vector<float>> onlineSoftmaxWithDot1PASS(
 
           pre_max_value = max_value;
           pre_sum = sum;
-
-          // LSE
-          sum_for_lse += std::exp(src[i]);
         }
-
-        float lse = logf(sum_for_lse);
-
-        lse_vector[loop] = lse;
         O_local_vector[loop] = O_local;
     }
 
-  return std::make_tuple(lse_vector, O_local_vector);
+  return std::make_tuple(lse, O_local_vector);
 }
 
 vector<float> flash_decoding(
@@ -117,44 +114,43 @@ vector<float> flash_decoding(
     const size_t loops = (seq_len + len_tile - 1) / len_tile;
 
     // 块1
-        vector<float> qk1(len_tile);
+    vector<float> qk1(len_tile);
 
-        // (1, size_per_head) * (size_per_head, len_tile) =  (1, len_tile)
-        for (size_t i = 0; i < len_tile; i++) {
-            qk1[i] = dot_product(q, k_mat[i]) / sqrt(d_k);
-        }
+    // (1, size_per_head) * (size_per_head, len_tile) =  (1, len_tile)
+    for (size_t i = 0; i < len_tile; i++) {
+        qk1[i] = dot_product(q, k_mat[i]) / sqrt(d_k);
+    }
 
-        // (1, len_tile) * (len_tile, size_per_head) = (1, size_per_head)
+    // (1, len_tile) * (len_tile, size_per_head) = (1, size_per_head)
 
-        // 切 v
-        std::vector<std::vector<float>> v1 = {v_mat[0], v_mat[1]};
-
-        auto [lse1, O_local1] =  onlineSoftmaxWithDot1PASS(qk1, v1);
+    // 切 v
+    std::vector<std::vector<float>> v1 = {v_mat[0], v_mat[1]};
+    auto [lse1, O_local1] =  onlineSoftmaxWithDot1PASS(qk1, v1);
 
     // 块2
-        vector<float> qk2(len_tile);
+    vector<float> qk2(len_tile);
 
-        // (1, size_per_head) * (size_per_head, len_tile) =  (1, len_tile)
-        for (size_t i = len_tile; i < 2 * len_tile; i++) {
-            qk2[i] = dot_product(q, k_mat[i]) / sqrt(d_k);
-        }
+    // (1, size_per_head) * (size_per_head, len_tile) =  (1, len_tile)
+    for (size_t i = len_tile; i < 2 * len_tile; i++) {
+        qk2[i-2] = dot_product(q, k_mat[i]) / sqrt(d_k);
+    }
 
-        // (1, len_tile) * (len_tile, size_per_head) = (1, size_per_head)
+    // (1, len_tile) * (len_tile, size_per_head) = (1, size_per_head)
 
-        // 切 v
-        std::vector<std::vector<float>> v2 = {v_mat[2], v_mat[3]};
+    // 切 v
+    std::vector<std::vector<float>> v2 = {v_mat[2], v_mat[3]};
 
-        auto [lse2, O_local2] =  onlineSoftmaxWithDot1PASS(qk2, v2);
+    auto [lse2, O_local2] =  onlineSoftmaxWithDot1PASS(qk2, v2);
 
     // reduction
-        vector<float> output(d_k);
+    vector<float> output(d_k);
 
-        for (int i = 0; i < d_k; i++) {
-            float LSE_final = logf(std::exp(lse1[i]) + std::exp(lse2[i]));
-            float O_final = std::exp(lse1[i] - LSE_final) * O_local1[i] + std::exp(lse2[i] - LSE_final) * O_local2[i];
+    for (int i = 0; i < d_k; i++) {
+        float LSE_final = logf(std::exp(lse1) + std::exp(lse2));
+        float O_final = std::exp(lse1 - LSE_final) * O_local1[i] + std::exp(lse2 - LSE_final) * O_local2[i];
 
-            output[i] = O_final;
-        }
+        output[i] = O_final;
+    }
 
     return output;
 }
@@ -162,12 +158,12 @@ vector<float> flash_decoding(
 // 测试
 int main() {
     // 假设维度为4，q_len = 1，k_len = 3
-    vector<float> q = {0.1, 0.2, 0.3, 0.4};
+    vector<float> q = {2.0f, 2.55f, 2.2f, 4.3f};
     vector<vector<float>> k = {
-        {0.2, 0.1, 0.1, 0.3},
-        {0.01, 0.3, 0.4, 0.1},
-        {0.1, 0.2, 0.1, 0.01},
-        {0.01, 0.2, 0.1, 0.3}
+        {2.2, 4.1, 2.1, 1.3},
+        {4.01, 1.3, 3.4, 2.1},
+        {2.1, 1.2, 3.1, 1.01},
+        {1.01, 4.2, 2.1, 2.3}
     };
     vector<vector<float>> v = {
         {1.0, 0.1, 0.1, 0.2},
